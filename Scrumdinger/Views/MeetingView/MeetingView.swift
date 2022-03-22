d108a73451e58cf50eac3fc5dd6bcd0e3ebbf4ef
asilencebtf <951208871@qq.com>
2022-03-22 21:26:59 +0800

    add feature
    SpeechRecognizer that transcribes live audio into written transcripts

--- Scrumdinger/Views/MeetingView/MeetingView.swift
+++ Scrumdinger/Views/MeetingView/MeetingView.swift
@@ -8,6 +8,8 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording: Bool = false;
     
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     var body: some View {
@@ -17,7 +19,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme, isRecording: isRecording)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
@@ -26,15 +28,20 @@ struct MeetingView: View {
         .navigationBarTitleDisplayMode(.inline)
         .onAppear() {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees);
-            scrumTimer.startScrum();
             scrumTimer.speakerChangedAction = {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecording = true
+            scrumTimer.startScrum();
         }
         .onDisappear() {
             scrumTimer.stopScrum()
-            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
+            speechRecognizer.stopTranscribing()
+            isRecording = false
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60, transcript: speechRecognizer.transcript)
             scrum.history.insert(newHistory, at: 0)
             
         }
